e04579f92fd06035dae3345392068311c6b00402
jejewitt <jejewitt@ualberta.ca>
2019-01-16 12:22:33 -0700

    added redirect for deep - may need to add it to other examples

--- server.py
+++ server.py
@@ -26,6 +26,7 @@ import os.path
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# curl -v -X -i GET http://127.0.0.1:8080/
 #https://stackoverflow.com/questions/41852380/how-to-abort-a-python-script-and-return-a-404-error
 
 
@@ -36,7 +37,8 @@ class MyWebServer(socketserver.BaseRequestHandler):
         error_405 = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html\r\n\r\n <html><head></head><body><h1>405 Method Not Allowed</h1></body></html>"
         request_200_html = "HTTP/1.1 200 OK \r\nContent-Type: text/html\r\n\r\n"
         request_200_css = "HTTP/1.1 200 OK \r\nContent-Type: text/css\r\n\r\n"
-        
+        request_301 = "HTTP/1.1 301 Moved Permanently \r\nLocation: "
+
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
 
@@ -47,7 +49,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
             path = 'www'+split[1]
 
 
-            if os.path.isfile(path):
+            if os.path.isfile(path) or os.path.isdir(path):
                 # print(split[1][-1:])
                 if split[1][-1:] == "/":
                     path = path + 'index.html'
@@ -63,7 +65,19 @@ class MyWebServer(socketserver.BaseRequestHandler):
                         # print(good)
                         self.request.sendall(bytearray(good,'utf-8'))
                     else:
-                        self.request.sendall(bytearray(error_404,'utf-8'))
+                    	#redirect if / is missing
+                    	print("e")
+                    	if os.path.isfile(path + '/index.html'):
+                    		# ***change to redirect
+                    		# this should work because it checks if there is 
+                    		#the file in the directory - it is likning
+                    		#to deep - but it the abs path isnt there it wont
+                    		#go into the if statement
+                    		print("it should be here ")
+                    		good = request_301+ path + "/ \r\n\r\n"+open(path+'/index.html','r').read()
+                    		self.request.sendall(bytearray(good,'utf-8'))
+                    	else:
+                        	self.request.sendall(bytearray(error_404,'utf-8'))
                 # else:
                 #     good = request_200_html+index_base.read()
                 #     self.request.sendall(bytearray(good,'utf-8'))
@@ -76,6 +90,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
                     self.request.sendall(bytearray(good,'utf-8'))
             else:
                 self.request.sendall(bytearray(error_404,'utf-8'))
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
